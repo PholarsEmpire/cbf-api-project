d317dbebe745d3707eef7500e3712ca04b89544e
TheAIbabe <miladebeautyfashion@gmail.com>
2025-08-23 06:07:36 +0100

    Handled all possible exceptions and also made my code cleaner

---
+++ src/main/java/com/folaolaitan/bondcatalog/customexceptions/BadRequestException.java
@@ -0,0 +1,8 @@
+package com.folaolaitan.bondcatalog.customexceptions;
+
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String message) {
+        super(message);
+    }
+
+}
